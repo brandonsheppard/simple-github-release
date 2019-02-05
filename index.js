735bf29a0807f43b7c74145159d248cd00e98569
philcon93 <phil@neto.com.au>
2019-02-05 13:30:22 +1000

    updated octokit to 16.15.0, and added conditional logic for release note information

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var GitHubApi = require('@octokit/rest')
+/* -------------- Setup -------------- */
+const Octokit = require('@octokit/rest')
 
+// Setup release data
+var releaseData = {
+	token: process.argv[2],
+	base: '',
+	repository: {
+		owner: process.argv[3],
+		name: process.argv[4]
+	},
+	'draft': process.argv[5] == 'draft' ? true : false,
+	name: ''
+}
+
+// authenticated requests, pass an auth token option to the Octokit constructor
+const octokit = new Octokit ({
+	auth: `token ${releaseData.token}`
+})
+
+/* -------------- Functions -------------- */
 // Pass commit data into this function to format a markdown commit note.
 // Data structure is based on what Github's compare endpoint returns.
 function formatNotes(data){
-	return data.map(commit => `- [:information_source:](${commit.html_url}) - ${commit.commit.message} - [${commit.author.login}](${commit.author.html_url})`).join('\n')
+	return data.map(commit =>
+		`- [:information_source:](${commit.html_url}) - ${commit.commit.message} ${commit.author ? `- [${commit.author.login}](${commit.author.html_url})` : ''}`
+	).join('\n')
 }
 
-// This function generates a date-based version number.
+// Generates a date-based version number.
 function generateVersionNumber(previousVersion){
 	let today = new Date();
 	var newMajor = `${today.getFullYear()}.${parseInt(today.getMonth()) + 1}`
@@ -22,48 +43,34 @@ function generateVersionNumber(previousVersion){
 	return newVersion;
 }
 
-/* --------------- */
-
-var github = new GitHubApi();
-var releaseData = {
-	token: process.argv[2],
-	base: '',
-	repository: {
-		owner: process.argv[3],
-		name: process.argv[4]
-	},
-	'draft': process.argv[5] == 'draft' ? true : false,
-	name: ''
-}
-
-github.authenticate({
-	type: 'token',
-	token: releaseData.token
-})
-
+// Sets releaseData base value
 function setBase(n){
 	releaseData.base = n
 	return n
 }
 
-github.repos.getLatestRelease({
+/* -------------- Release -------------- */
+octokit.repos.getLatestRelease({
 	owner: releaseData.repository.owner,
 	repo: releaseData.repository.name
-}).then(res =>
-	github.repos.compareCommits({
+})
+.then(res =>
+	octokit.repos.compareCommits({
 		owner: releaseData.repository.owner,
 		repo: releaseData.repository.name,
 		head: 'master',
 		base: setBase(res.data.tag_name)
-	}).then(res =>
-		github.repos.createRelease({
+	})
+	.then(res =>
+		octokit.repos.createRelease({
 			owner: releaseData.repository.owner,
 			repo: releaseData.repository.name,
 			tag_name: generateVersionNumber(releaseData.base),
 			name: generateVersionNumber(releaseData.base) + ' ' + releaseData.name,
 			draft: releaseData.draft,
 			body: `# All Changes \n${formatNotes(res.data.commits)}`
-		}).then(res =>
+		})
+		.then(res =>
 			console.log(res)
 		)
 	)
